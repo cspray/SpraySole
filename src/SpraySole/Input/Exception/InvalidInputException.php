3048656513b02a7ee4b5e1a0b9a4e140c17932e0
Charles Sprayberry <cspray@gmail.com>
2013-08-13 22:36:42 -0400

    adding an exception for invalid input when parsing

---
+++ src/SpraySole/Input/Exception/InvalidInputException.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * An exception thrown if the input provided from the console is invalid.
+ * 
+ * @author Charles Sprayberry
+ * @license See LICENSE in source root
+ */
+
+namespace SpraySole\Input\Exception;
+
+use \InvalidArgumentException;
+
+class InvalidInputException extends InvalidArgumentException {}
