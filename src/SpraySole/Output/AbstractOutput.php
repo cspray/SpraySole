952c869bc6afd49111cb1fc27588c2c1ed40b0d1
Charles Sprayberry <cspray@gmail.com>
2013-08-14 23:55:11 -0400

    adding some docblocks

--- src/SpraySole/Output/AbstractOutput.php
+++ src/SpraySole/Output/AbstractOutput.php
@@ -1,6 +1,7 @@
 <?php
 
 /**
+ * Simple abstract class providing support for getting and setting an Output\Formatter
  * 
  * @author Charles Sprayberry
  * @license See LICENSE in source root
@@ -8,16 +9,23 @@
 
 namespace SpraySole\Output;
 
-
 abstract class AbstractOutput implements Output {
 
-
+    /**
+     * @property \SpraySole\Output\Formatter
+     */
     private $Formatter;
 
+    /**
+     * @param \SpraySole\Output\Formatter $Formatter
+     */
     public function setFormatter(Formatter $Formatter) {
         $this->Formatter = $Formatter;
     }
 
+    /**
+     * @return \SpraySole\Output\Formatter
+     */
     public function getFormatter() {
         return $this->Formatter;
     }
