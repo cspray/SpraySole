d4582177b3e532a0c6c509ce5764cd7c34085e6c
Charles Sprayberry <cspray@gmail.com>
2013-08-14 23:56:22 -0400

    adding test for StreamOutput test

---
+++ tests/SpraySoleTest/Output/StreamOutputTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * 
+ * @author Charles Sprayberry
+ * @license See LICENSE in source root
+ */
+
+namespace SpraySoleTest\Output;
+
+use \SpraySole\Output\StreamOutput;
+use \SpraySoleTest\Stubs\StreamStub;
+
+// registering the wrapper does not trigger autoloading so we have to manually include the file
+require_once \SPRAYSOLE_ROOT . '/tests/SpraySoleTest/Stubs/StreamStub.php';
+
+class StreamOutputTest extends \PHPUnit_Framework_TestCase {
+
+    public static function setUpBeforeClass() {
+        \stream_wrapper_register('spraysoletest', '\\SpraySoleTest\\Stubs\\StreamStub');
+    }
+
+    public function testStreamOutputWritingMessageWithoutNewLinePassingStringStreamName() {
+        $Output = new StreamOutput('spraysoletest://whatever');
+        $Output->write('test message');
+
+        $actual = StreamStub::$body;
+        $this->assertSame('test message', $actual);
+    }
+
+    public static function tearDownAfterClass() {
+        \stream_wrapper_unregister('spraysoletest');
+    }
+
+
+
+
+}
