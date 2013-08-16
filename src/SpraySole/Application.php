3294b8d361fb59f884e48272ab8fbd853dc9f362
Charles Sprayberry <cspray@gmail.com>
2013-08-15 22:10:40 -0400

    implementing adding and getting commands

--- src/SpraySole/Application.php
+++ src/SpraySole/Application.php
@@ -15,22 +15,40 @@ use \SpraySole\Command\Command;
 
 class Application {
 
+    private $commands = [];
+
     /**
+     * Provide a Command\Command that can be ran from your console app.
+     *
      * @param \SpraySole\Command\Command $Command
      * @return void
      */
     public function addCommand(Command $Command) {
-
+        $this->commands[$Command->getName()] = $Command;
     }
 
     /**
-     *
+     * Determination based on the name of the command; if a string is passed it
+     * is assumed to be the command name and if a Command\Command implementation
+     * is passed the Command::getName will be used.
      *
      * @param string|Command $command
      * @return boolean
      */
     public function hasCommand($command) {
 
+    }
+
+    /**
+     * Return a collection of Command\Command implementations added to the Application.
+     *
+     * The collection should be returned so that when iterating over the collection
+     * the name of the Command is the key while the Command itself is the value.
+     *
+     * @return \SpraySole\Command\Command[]
+     */
+    public function getCommands() {
+        return $this->commands;
     }
 
     /**
