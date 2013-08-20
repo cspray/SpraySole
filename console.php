08568337c74d66c433a54ed4af425ec8980ccb23
Charles Sprayberry <cspray@gmail.com>
2013-08-19 20:33:03 -0400

    causing the console.php to run the app with appropriate input and output

--- console.php
+++ console.php
@@ -11,8 +11,13 @@ if (\file_exists('vendor/autoload.php')) {
 }
 
 use \SpraySole\Application;
+use \SpraySole\Input\ArgvInput;
+use \SpraySole\Output\StreamOutput;
 
 $App = new Application();
 
+$Input = new ArgvInput($argv);
+$StdOut = new StreamOutput('php://stdout');
+$StdErr = new StreamOutput('php://stderr');
 
-echo 'zomg nothing yet' . \PHP_EOL;
+$App->run($Input, $StdOut, $StdErr);
