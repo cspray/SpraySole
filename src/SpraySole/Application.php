2001fbf227cb720644636466437fd3db024de091
Charles Sprayberry <cspray@gmail.com>
2013-08-30 08:44:43 -0400

    UPD adding code to pass all tests

--- src/SpraySole/Application.php
+++ src/SpraySole/Application.php
@@ -15,16 +15,19 @@ use \SpraySole\Command\Command;
 
 class Application {
 
+    const SPRAYSOLE_VERSION = '0.1.0alpha';
+
     private $commands = [];
 
     /**
      * Provide a Command\Command that can be ran from your console app.
      *
      * @param \SpraySole\Command\Command $Command
-     * @return void
+     * @return \SpraySole\Application
      */
     public function addCommand(Command $Command) {
         $this->commands[$Command->getName()] = $Command;
+        return $this;
     }
 
     /**
@@ -64,17 +67,58 @@ class Application {
      * @return integer
      */
     public function run(Input $Input, Output $StdOut, Output $StdErr = null) {
+        if ($version = $Input->getOption('version')) {
+            $StdOut->write($this->getVersionMessage(), Output::APPEND_NEW_LINE);
+            return;
+        }
+
         if (!$Input->argumentsCount()) {
             $StdOut->write($this->getUsageMessage(), Output::APPEND_NEW_LINE);
+            return;
         }
+
+        $cmdName = $Input->getArgument(0);
+        if (!$this->hasCommand($cmdName)) {
+            $StdOut->write($this->getCommandNotFoundMessage($cmdName), Output::APPEND_NEW_LINE);
+            return;
+        }
+
+        $StdErr = ($StdErr) ?: $StdOut;
+        return $this->getCommands()[$cmdName]->execute($Input, $StdOut, $StdErr);
     }
 
+    /**
+     * @return string
+     */
     private function getUsageMessage() {
         return <<<TEXT
 No command was provided
 
 usage:
     [options] command [arguments]
+TEXT;
+    }
+
+    /**
+     * @return string
+     */
+    private function getVersionMessage() {
+        $version = self::SPRAYSOLE_VERSION;
+        return <<<TEXT
+SpraySole version {$version}
+    A console driven application powered by PHP 5.4+
+TEXT;
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     */
+    private function getCommandNotFoundMessage($name) {
+        return <<<TEXT
+Command, {$name}, could not be found.
+
+Please use --help or --help <command> for more information on using this application.
 TEXT;
 
     }
