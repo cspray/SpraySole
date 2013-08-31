0f7ca89ee6a2f410aa20dc56b4d9b8e1a68637b9
Charles Sprayberry <cspray@gmail.com>
2013-08-31 11:27:14 -0400

    NEW empty ls command

---
+++ src/SpraySole/Command/Ls.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * A Command provided "out of the box" that will list all added Command implementations
+ * and their description.
+ * 
+ * @author  Charles Sprayberry
+ * @license See LICENSE in source root
+ * @version 0.1
+ * @since   0.1
+ */
+
+namespace SpraySole\Command;
+
+
+use SpraySole\Input\Input;
+use SpraySole\Output\Output;
+
+class Ls extends AbstractCommand {
+
+    /**
+     * Return the description of the command.
+     *
+     * @return string
+     */
+    public function getDescription() {
+
+    }
+
+    /**
+     * Return the help output for the command.
+     *
+     * @return string
+     */
+    public function getHelp() {
+
+    }
+
+    /**
+     * Execute the given Command based on the Input, writing whatever Output is
+     * necessary based on any normal processing or errors; return an exit code
+     * or 0 if there was no error.
+     *
+     * @param \SpraySole\Input\Input $Input
+     * @param \SpraySole\Output\Output $StdOut
+     * @param \SpraySole\Output\Output $StdErr
+     * @return integer
+     */
+    public function execute(Input $Input, Output $StdOut, Output $StdErr) {
+
+    }
+}
