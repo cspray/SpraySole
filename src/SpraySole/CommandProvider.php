ead1961a0648ba39e3617c3d547d0e35eb5b7e01
Charles Sprayberry <cspray@gmail.com>
2013-08-31 11:09:26 -0400

    NEW interface for providers of Commands to and Application

---
+++ src/SpraySole/CommandProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * An interface that represents a type capable of providing one or more Command
+ * implementations to an Application.
+ * 
+ * @author  Charles Sprayberry
+ * @license See LICENSE in source root
+ * @version 0.1
+ * @since   0.1
+ */
+
+namespace SpraySole;
+
+use \SpraySole\Application;
+
+interface CommandProvider {
+
+    /**
+     * Add the appropriate Command implementations to the Application.
+     *
+     * @param \SpraySole\Application $App
+     * @return void
+     */
+    public function register(Application $App);
+
+}
