1aabbc49d2d613bc1d74f8d2779a3ae60390d316
Charles Sprayberry <cspray@gmail.com>
2013-08-31 11:07:09 -0400

    NEW library specific test case to help ease the amount of typing when mocking

---
+++ tests/SpraySoleTest/Unit/TestCase.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 
+ * @author Charles Sprayberry
+ * @license See LICENSE in source root
+ */
+
+namespace SpraySoleTest\Unit;
+
+use \PHPUnit_Framework_TestCase;
+
+class TestCase extends PHPUnit_Framework_TestCase {
+
+    private $mocks = [
+        'Input' => '\\SpraySole\\Input\\Input',
+        'Output' => '\\SpraySole\\Output\\Output',
+        'Formatter' => '\\SpraySole\\Output\\Formatter',
+        'Command' => '\\SpraySole\\Command\\Command',
+        'Provider' => '\\SpraySole\\CommandProvider',
+        'Application' => '\\SpraySole\\Application'
+    ];
+
+    protected function mocks($interface) {
+        return isset($this->mocks[$interface]) ? $this->mocks[$interface] : null;
+    }
+
+}
