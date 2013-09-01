bbb172d1e513eb08a083750c91d6a2da6fc23e35
Charles Sprayberry <cspray@gmail.com>
2013-09-01 12:22:58 -0400

    NEW adding exception for a resource file being invalid

---
+++ src/SpraySole/Command/Exception/InvalidResourceFileException.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Exception thrown if a resource file is invalid for some reason.
+ * 
+ * @author  Charles Sprayberry
+ * @license See LICENSE in source root
+ * @version 0.1
+ * @since   0.1
+ */
+
+namespace SpraySole\Command\Exception;
+
+use \InvalidArgumentException;
+
+class InvalidResourceFileException extends InvalidArgumentException {}
