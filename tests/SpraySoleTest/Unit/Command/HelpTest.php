b507b7a8a2878edffa3005c0aefadcb34cd76cb2
Charles Sprayberry <cspray@gmail.com>
2013-09-01 12:25:29 -0400

    UPD adding some tests for working with resource files and getting a listed command's help

--- tests/SpraySoleTest/Unit/Command/HelpTest.php
+++ tests/SpraySoleTest/Unit/Command/HelpTest.php
@@ -23,12 +23,12 @@ class HelpTest extends Unit\TestCase {
     }
 
     public function testGettingHelpCommandName() {
-        $Cmd = new Help($this->CmdConfig);
+        $Cmd = new Help($this->CmdConfig, '');
         $this->assertSame('help', $Cmd->getName());
     }
 
     public function testGettingHelpDescription() {
-        $Cmd = new Help($this->CmdConfig);
+        $Cmd = new Help($this->CmdConfig, '');
         $expected = <<<TEXT
 Provide details on how to use SpraySole or a specific command.
 TEXT;
@@ -42,13 +42,20 @@ TEXT;
               ->with(1)
               ->will($this->returnValue(null));
 
+        $expected = <<<TEXT
+all the things
+
+TEXT;
+
+
         // We are using different output mocks to make sure we are writing to stdout and not stderr
         $StdOut = $this->getMock($this->mocks('Output'));
         $StdOut->expects($this->once())
-               ->method('write');
+               ->method('write')
+               ->with($expected, true);
         $StdErr = $this->getMock($this->mocks('Output'));
 
-        $Cmd = new Help($this->CmdConfig);
+        $Cmd = new Help($this->CmdConfig, \SPRAYSOLE_ROOT . '/tests/SpraySoleTest/_resources/spraysole.txt');
         $code = $Cmd->execute($Input, $StdOut, $StdErr);
         $this->assertSame(0, $code, 'The error code is indicative of an error');
     }
@@ -63,12 +70,52 @@ TEXT;
         // We are using different output mocks to make sure we are writing to stdout and not stderr
         $StdOut = $this->getMock($this->mocks('Output'));
         $StdOut->expects($this->once())
-               ->method('write');
+               ->method('write')
+               ->with('ran it' . \PHP_EOL);
         $StdErr = $this->getMock($this->mocks('Output'));
 
-        $Cmd = new Help($this->CmdConfig);
+        $CmdConfig = new Config([
+            Config::NAME_PARAM => 'help',
+            Config::HELP_FILE_PARAM => \SPRAYSOLE_ROOT . '/tests/SpraySoleTest/_resources/help.txt'
+        ]);
+        $Cmd = new Help($CmdConfig, '');
         $code = $Cmd->execute($Input, $StdOut, $StdErr);
         $this->assertSame(0, $code, 'The error code is indicative of an error');
+    }
+
+    public function testGetAddedCommandHelp() {
+        $Input = $this->getMock($this->mocks('Input'));
+        $Input->expects($this->once())
+              ->method('getArgument')
+              ->with(1)
+              ->will($this->returnValue('foo'));
+
+        $StdOut = $this->getMock($this->mocks('Output'));
+        $StdOut->expects($this->once())
+               ->method('write')
+               ->with('foo command help');
+        $StdError = $this->getMock($this->mocks('Output'));
+
+        $App = $this->getMock($this->mocks('Application'));
+        $App->expects($this->once())
+            ->method('hasCommand')
+            ->with('foo')
+            ->will($this->returnValue(true));
+
+        $FooCmd = $this->getMock($this->mocks('Command'));
+        $FooCmd->expects($this->once())
+               ->method('getHelp')
+               ->will($this->returnValue('foo command help'));
+
+        $App->expects($this->once())
+            ->method('getCommands')
+            ->will($this->returnValue(['foo' => $FooCmd]));
+
+        $Cmd = new Help($this->CmdConfig, '');
+        $Cmd->setApplication($App);
+        $code = $Cmd->execute($Input, $StdOut, $StdError);
+
+        $this->assertSame(0, $code);
     }
 
 
